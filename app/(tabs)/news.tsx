3754ceb70072e8626fae8dbfd459393cdcb3d359
Iago Munoz <iago.rmunoz@gmail.com>
2024-09-11 19:05:26 -0300

    testando iamgem de noticias

--- app/(tabs)/news.tsx
+++ app/(tabs)/news.tsx
@@ -11,11 +11,14 @@ export default function TabTwoScreen() {
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage} />}>
+      headerImage={
+      <Image
+        source={require('@/assets/images/amongNews.jpg')}
+      />}>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Explore</ThemedText>
+        <ThemedText type="title">Noticias do momento</ThemedText>
       </ThemedView>
-      <ThemedText>This app includes example code to help you get started.</ThemedText>
+      <ThemedText>Fique por dentro das informações mais suspeitas de toda a galaxia</ThemedText>
       <Collapsible title="File-based routing">
         <ThemedText>
           This app has two screens:{' '}
