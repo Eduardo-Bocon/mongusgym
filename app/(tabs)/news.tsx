9207a2e1bd43eb33db1454e0170db735aae33e48
Iago Munoz <iago.rmunoz@gmail.com>
2024-09-11 19:15:30 -0300

    redefinindo nome do amongnews

--- app/(tabs)/news.tsx
+++ app/(tabs)/news.tsx
@@ -13,7 +13,7 @@ export default function TabTwoScreen() {
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
       headerImage={
         <Image
-          source={require('@/assets/images/amongusIndex.jpeg')}
+          source={require('@/assets/images/amongNews.jpeg')}
           style={styles.mongusImage}
         />}>
       <ThemedView style={styles.titleContainer}>
